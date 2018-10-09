2b064fc9f0f87bed9ea603a9cb161b069dcf467f
peace <golpeace@163.com>
2018-10-09 15:05:46 +0800

    hot fix commit

--- src/com/cdp/CdpDEmo.java
+++ src/com/cdp/CdpDEmo.java
@@ -3,5 +3,6 @@ package com.cdp;
 public class CdpDEmo {
 	public static void main(String[] args) {
 		System.out.println("µÚ¶þ¸ö²âÊÔ"+"zuobianxiugai");
+		System.out.println("hot fix fenzhi ");
 	}
 }
